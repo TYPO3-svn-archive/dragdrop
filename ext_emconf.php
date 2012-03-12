f532f213a8839dcc72fdcbbc43c06a888d1e9ba9
professorweb <professorweb@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-03-12 15:20:33 +0000

    Version 0.4.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/dragdrop/trunk@59146 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "dragdrop".
 #
-# Auto generated 05-03-2012 10:30
+# Auto generated 12-03-2012 16:20
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '0.3.1',
+	'version' => '0.4.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.5.0-0.0.0',
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'suggests' => array(
 	),
-	'_md5_values_when_last_written' => 'a:8:{s:12:"ext_icon.gif";s:4:"269d";s:17:"ext_localconf.php";s:4:"105a";s:14:"ext_tables.php";s:4:"0643";s:14:"ext_tables.sql";s:4:"d41d";s:29:"Classes/class.tx_dragdrop.php";s:4:"9fe2";s:41:"Classes/XClass/class.ux_tx_cms_layout.php";s:4:"5070";s:33:"Resources/Public/CSS/dragdrop.css";s:4:"1553";s:39:"Resources/Public/JavaScript/dragdrop.js";s:4:"b050";}',
+	'_md5_values_when_last_written' => 'a:9:{s:12:"ext_icon.gif";s:4:"269d";s:17:"ext_localconf.php";s:4:"105a";s:14:"ext_tables.php";s:4:"0643";s:14:"ext_tables.sql";s:4:"d41d";s:29:"Classes/class.tx_dragdrop.php";s:4:"9fe2";s:41:"Classes/XClass/class.ux_tx_cms_layout.php";s:4:"5070";s:33:"Resources/Public/CSS/dragdrop.css";s:4:"0af6";s:36:"Resources/Public/Images/drophere.gif";s:4:"a613";s:39:"Resources/Public/JavaScript/dragdrop.js";s:4:"7003";}',
 );
 
 ?>
